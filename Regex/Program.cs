d770aa66f615b3d2c0dc5f7f4f0e9d07b7fe7035
SitongChe <chesitong@126.com>
2023-06-08 16:32:41 -0700

    Update Program.cs

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -38,18 +38,36 @@ using System.Xml;
 public partial class Program
 {
 
-    private const string ReportColumnGroup = "ReportColumn";
+    
     static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
+
+    //example 1 0.44 ratio
+    private const string ReportColumnGroup = "ReportColumn";
     private static readonly string s_haystack = " sdf Entity.ReportColumn test string that contains some {= Feed. Attribute1 :d} d {= Feed . Attribute2 :$19.95} d {= Feed.Attribute3:b} f {= Feed  .  Attribute4  :a} rem {  COUNTDOWN(2023-09-01 11:11:11) )  }";
 
     private static Regex reportColumnRegex = new Regex($@"(\w+\.)?(?<{ReportColumnGroup}>\w+)");
-
     [GeneratedRegex($@"(\w+\.)?(?<{ReportColumnGroup}>\w+)")]
     private static partial Regex ReportColumnRegex();
 
     [Benchmark(Baseline = true)] public string Interpreter() => reportColumnRegex.Replace(s_haystack, " ");
 
     [Benchmark] public string SourceGenerator() => ReportColumnRegex().Replace(s_haystack, " ");
+
+    //example 2  0.37 ratio
+    private static readonly string emails = " sitche@microsoft.com";
+
+    private static Regex emailMatch = new Regex(
+        @"^(([^<>()[\]\\.,;:\s@""]+(\.[^<>()[\]\\.,;:\s@""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$",
+        RegexOptions.IgnoreCase);
+    [GeneratedRegex(
+        @"^(([^<>()[\]\\.,;:\s@""]+(\.[^<>()[\]\\.,;:\s@""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$",
+        RegexOptions.IgnoreCase)]
+    private static partial Regex EmailMatch();
+
+    [Benchmark] public bool Interpreter2() => emailMatch.IsMatch(emails);
+
+    [Benchmark] public bool SourceGenerator2() => EmailMatch().IsMatch(emails);
+
 
 }
